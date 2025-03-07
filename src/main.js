b9b2b1db9e9cb17e5b69836c137081105b29295a
Marcos-Monte <marcos.monte.junior@gmail.com>
2025-03-07 15:11:55 -0300

    feat: adiciona Bootstrap e BootstrapVue, remove páginas About e Home, e atualiza estrutura do App; add sass, sass-loader

--- src/main.js
+++ src/main.js
@@ -1,8 +1,17 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue';
+import Vue from 'vue';
+import App from './App.vue';
+import './globals.scss';
+import router from './router';
+
+// Importe os arquivos CSS Bootstrap e BootstrapVue (a ordem é importante) 
+import 'bootstrap-vue/dist/bootstrap-vue.css';
+import 'bootstrap/dist/css/bootstrap.css';
 
 Vue.config.productionTip = false
+
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
 
 new Vue({
   router,
