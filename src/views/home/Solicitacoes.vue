e45263ad722ad9fdd2cdb8db571297f63f4241d8
Marcos-Monte <marcos.monte.junior@gmail.com>
2025-03-17 22:26:55 -0300

    feat: reformula a estrutura da página de solicitações; adiciona novos elementos de navegação e melhora o layout

--- src/views/home/Solicitacoes.vue
+++ src/views/home/Solicitacoes.vue
@@ -1,10 +1,33 @@
 <template>
 
-    <b-row class="justify-content-center align-items-center vh-100" >
+    <b-container fluid class="contentBox flex-column justify-content-center align-items-center" >
 
-        Solicitacoes de Reembolsos
+        <b-row fluid no-gutters >
+            <b-col cols="12" class="pathBox d-flex justify-content-start align-items-center gap-3">
+                <img src="../../assets/path/iconHomePath.png" alt="Icone de uma casinha">
+                <img src="../../assets/path/iconArrowPath.png" alt="Icone de uma seta">
+                <p class="m-0">Reembolsos</p>
+            </b-col>
+        </b-row>
 
-    </b-row>
+        <b-container fluid class="d-flex flex-column justify-content-between align-items-center vh-100 box">
+
+            <b-row>
+                Forms
+            </b-row>
+
+            <b-row>
+                Table
+            </b-row>
+
+            <b-row >
+                Footer
+            </b-row>
+
+        </b-container>
+
+
+    </b-container>
 
 </template>
 
@@ -22,5 +45,26 @@ import NavBar from '@/components/NavBar.vue';
 
 <style scoped lang="scss">
 
+.contentBox {
+    width: 100%;
+    height: 100vh !important;
+    padding-left: 5rem 0 0 0!important;
+    gap: 3rem;
+
+    h2 {
+        font-weight: 700;
+    }
+}
+
+.pathBox{
+    position: absolute;
+    top:0;
+    left: 8rem;
+    padding: 1rem 0;
+}
+
+.box{
+    padding-top: 4rem !important;
+}
 
 </style>
