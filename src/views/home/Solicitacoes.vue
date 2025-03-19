eb1bf14467962a085ce1367651ec46bd29db6574
Marcos-Monte <marcos.monte.junior@gmail.com>
2025-03-18 22:39:27 -0300

    feat: refatora formulário de solicitações; altera modelo de dados, adiciona funcionalidade de carregamento e limpeza de registros

--- src/views/home/Solicitacoes.vue
+++ src/views/home/Solicitacoes.vue
@@ -1,6 +1,6 @@
 <template>
 
-    <b-container fluid class="contentBox flex-column justify-content-center align-items-center" >
+    <b-container fluid class="contentBox flex-column justify-content-start align-items-center" >
 
         <b-row fluid no-gutters >
             <b-col cols="12" class="pathBox d-flex justify-content-start align-items-center gap-3">
@@ -19,12 +19,12 @@
 
                         <div class="col">
                             <label for="name" class="form-label">Nome Completo</label>
-                            <input type="text" class="form-control" aria-label="name" required v-model="register.name">
+                            <input type="text" class="form-control" aria-label="name" required v-model="form.name">
                         </div>
 
                         <div class="col col-12 col-md-4">
                             <label for="enterprise" class="form-label">Empresa</label>
-                            <select class="form-select" aria-label="enterprise" required v-model="register.enterprise">
+                            <select class="form-select" aria-label="enterprise" required v-model="form.enterprise">
                                 <option selected value="">Selecione</option>
                                 <option value="A">Empresa A</option>
                                 <option value="B">Empresa B</option>
@@ -35,7 +35,7 @@
 
                         <div class="col-12">
                             <label for="description" class="form-label">Motivo do Reembolso</label>
-                            <textarea class="form-control" id="description" rows="3" placeholder="Descreva o motivo do reembolso..." v-model="register.description"></textarea>
+                            <textarea class="form-control" id="description" rows="3" placeholder="Descreva o motivo do reembolso..." v-model="form.description"></textarea>
                         </div>
                     </div>
                 </div>
@@ -48,12 +48,12 @@
 
                         <div class="col col-12 col-lg-4">
                             <label for="date" class="form-label">Data</label>
-                            <input type="date" class="form-control"  aria-label="date" required v-model="register.date">
+                            <input type="date" class="form-control"  aria-label="date" required v-model="form.date">
                         </div>
 
                         <div class="col col-12 col-lg-4">
                             <label for="expenseType" class="form-label">Tipo de Despesa</label>
-                            <select class="form-select" aria-label="expenseType" required v-model="register.expenseType">
+                            <select class="form-select" aria-label="expenseType" required v-model="form.expenseType">
                                 <option selected value="">Selecione</option>
                                 <option value="alimentacao">Alimentação</option>
                                 <option value="conducao">Condução</option>
@@ -66,7 +66,7 @@
 
                         <div class="col col-12 col-lg-3">
                                 <label for="currency" class="form-label">Moeda</label>
-                                <select class="form-select" aria-label="currency" required v-model="register.currency">
+                                <select class="form-select" aria-label="currency" required v-model="form.currency">
                                     <option selected value="">Selecione</option>
                                     <option value="brl">BRL</option>
                                     <option value="usd">USD</option>
@@ -78,7 +78,7 @@
                         
                         <div class="col-12">
                             <label for="expenseValue" class="form-label">Total Despesa</label>
-                            <input class="form-control" id="expenseValue" placeholder="Insirerir o valor da despesa" v-model="register.expenseValue"></input>
+                            <input type="number" class="form-control" id="expenseValue" placeholder="Insirerir o valor da despesa" v-model="form.expenseValue"></input>
                         </div>
                         
                         <b-col cols="12" class="p-0 d-flex justify-content-end align-items-center gap-2">
@@ -91,7 +91,7 @@
                             </b-button>
                         
                             <!-- Botão para criar conta -->
-                            <b-button class="btn-primary-light">
+                            <b-button class="btn-primary-light" @click="clearForm">
                                 <img src="../../assets/reembolsos/iconApagar.png" alt="Ícone de apagar Registro">
                             </b-button>
                         </b-col>
@@ -102,9 +102,33 @@
                 
             </div>
 
-            <b-row>
-                Table
-            </b-row>
+            <div class="row w-100 h-100 d-flex flex-column justify-content-start align-items-center tableBox">
+
+                <template v-if="computedRegisters.length > 0">
+                    <b-table
+                        reponsive
+                        :items="computedRegisters"
+                        :per-page="10"
+                        :current-page="currentPage"
+                        aria-controls="my-table"
+                    ></b-table>
+
+                    <b-pagination
+                        v-if="computedRegisters.length > 10"
+                        first-number
+                        align="fill"
+                        v-model="currentPage"
+                        :total-rows="rows"
+                        :per-page="10"
+                        aria-controls="my-table"
+                    ></b-pagination>
+                </template>
+
+                <template v-else>
+                    <h3 class="text-center">Não há registros!</h3>
+                </template>
+
+            </div>
 
             <b-row >
                 Footer
@@ -127,15 +151,19 @@ import NavBar from '@/components/NavBar.vue';
 
         data(){
             return {
-                register: {
+                form: {
                     name: '',
                     enterprise: '',
                     description: '',
                     date: '',
                     expenseType: '',
                     currency: '',
-                    expenseValue: '',
-                }
+                    expenseValue: ''
+                },
+
+                registers: [],
+
+                currentPage: 1,
             }
         },
 
@@ -143,13 +171,58 @@ import NavBar from '@/components/NavBar.vue';
             postRegister(){
 
                 try{
+                
+                    this.registers.push({...this.form});
 
-                    console.log('Registro: ', this.register)
+                    localStorage.setItem('registers', JSON.stringify(this.registers));
+
+                    this.form = {};
 
 
                 } catch(error){
                     console.error('Erro ao registrar, tente novamente!', error)
                 }
+            },
+
+            loadRegisters(){
+                try {
+                    const savedRegisters = localStorage.getItem('registers');
+
+                    if (savedRegisters){
+                        this.registers = JSON.parse(savedRegisters)
+                    }
+
+                    console.log('Registros: ', this.registers)
+
+                } catch(error){
+                    console.error('Erro ao carregar Registros!', error)
+                }
+            }, 
+
+            clearForm(){
+                this.form = {
+                    name: '',
+                    enterprise: 'Selecione',
+                    description: '',
+                    date: '',
+                    expenseType: 'Selecione',
+                    currency: 'Selecione',
+                    expenseValue: ''
+                }
+            }
+        },
+
+        mounted(){
+            this.loadRegisters(); // Carrega registros salvos ao iniciar a aplicação
+        },
+
+        computed: {
+            computedRegisters(){
+                return this.registers
+            },
+
+            rows(){
+                return this.registers.length
             }
         }
 
@@ -183,14 +256,17 @@ import NavBar from '@/components/NavBar.vue';
 
 .formBox {
     padding-left: 5rem;
-    //background-color: red;´
     display: flex;
     align-items: center;
-    //gap: .5rem;
+
+}
+
+.tableBox{
+    padding-left: 8rem;
 }
 
 @media (max-width: 768px){
-    .formBox{
+    .formBox, .tableBox{
         padding-left: 0;
     }
 }
